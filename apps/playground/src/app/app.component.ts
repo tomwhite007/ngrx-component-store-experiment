9e36f7564b3f5e4ddd4f89fa7de4e7c735c49149
Tom White <tomwhite007@gmail.com>
2020-11-09 07:54:16 +0000

    factor state into service

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -1,45 +1,30 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store';
 import { BooksFacade } from './+state/books.facade';
 import { BooksEntity } from './+state/books.models';
-
-interface LocalState {
-  showForm: boolean;
-  selectedTab: number;
-}
+import { AppComponentStateService } from './app-component-state.service';
 
 @Component({
   selector: 'playground-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [AppComponentStateService],
 })
 export class AppComponent {
-  readonly vm$ = this.componentStore.select(
-    this.componentStore.select((state) => state),
-    this.books.allBooks$,
-    (LocalState, allBooks) => ({ ...LocalState, allBooks })
-  );
+  vm$ = this.localState.vm$;
 
   constructor(
     private books: BooksFacade,
-    private componentStore: ComponentStore<LocalState>
-  ) {
-    this.componentStore.setState({
-      showForm: false,
-      selectedTab: 0,
-    });
+    private localState: AppComponentStateService
+  ) {}
+
+  toggleShowForm() {
+    this.localState.toggleShowForm();
   }
 
-  readonly showFormToggle = this.componentStore.updater((state) => ({
-    ...state,
-    showForm: !state.showForm,
-  }));
-
-  readonly selectTab = this.componentStore.updater((state, tabNo: number) => ({
-    ...state,
-    selectedTab: tabNo,
-  }));
+  selectTab(tabNo: number) {
+    this.localState.selectTab(tabNo);
+  }
 
   upsertBook(book: BooksEntity) {
     this.books.upsertBook(book);
